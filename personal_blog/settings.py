862a2c421be7691ced50b1da97f7a185154b09d3
Neeraj Madake <neerajmadake18@gmail.com>
2024-10-25 15:45:37 +0530

    whitenoise config

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -54,10 +54,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'personal_blog.urls'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
     {
