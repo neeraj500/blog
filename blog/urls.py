9238c833553c4a9a193f4356c211b72f540ce962
Neeraj Madake <neerajmadake18@gmail.com>
2024-11-04 15:14:47 +0530

    add tailwind config, change ui

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("category/<category>/", views.blog_category, name="blog_category"),
+     path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+if settings.DEBUG:                                                                  
+   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
