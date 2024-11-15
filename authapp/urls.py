6a7e144876a8e8d0235289d2b3fa0b09c5c582f3
Neeraj Madake <neerajmadake18@gmail.com>
2024-11-15 23:24:27 +0530

    style login and signup pages

--- authapp/urls.py
+++ authapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import authView, home
 
 urlpatterns = [
+    path("login/", home, name="login"),
     path("signup/", authView, name="signup"),
     path("accounts/", include("django.contrib.auth.urls"))
 ]
